be772085f5a3036805814806eaa670426dc974f3
philipearnshaw <philip.earnshaw@sky.uk>
2018-08-13 16:52:16 +0100

    Added POST todos route and refactored mongoose

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+// Configure the middleware
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.listen(port, () => {console.log(`Server up on port ${port}`)});
